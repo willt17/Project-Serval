e8da154cdca594747ce78928f3d0e53aa72e07dd
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-03-25 21:31:35 -0500

    Updated the readme as well as started the primary logic.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project_Serval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] riderNumbers = new string[999];
+            string[] riderNames = new string[999];
+        }
+        static void ImportRiders(string[] riderNumbers, string[] riderNames)
+        {
+            int count = 0;
+            StreamReader inFile = new StreamReader("testFile.Txt");
+            string[] temp = inFile.ReadLine();
+            while (temp != null)
+            {
+                riderNumbers[count] = temp[0];
+                riderNames[count] = temp[1];
+                temp = inFile.ReadLine();
+                count++;
+            }
+        }
+    }
+}
