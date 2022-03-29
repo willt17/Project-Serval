1a82a2e6ba0f4f4e5f5790e645d840a5cc9e65e9
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-03-29 00:45:18 -0500

    Added a new custom class file.

---
+++ riders.cs
@@ -0,0 +1,10 @@
+namespace Project_Serval
+{
+    public class riders
+    {
+        public int riderNumber;
+        public string firstName;
+        public string lastName;
+        public string riderClass;
+    }
+}
