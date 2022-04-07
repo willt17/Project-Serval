531fff9151509d4cacd9c4caa4e4c974e68bd715
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-04-07 15:16:57 -0500

    Added getters and setters withing riders.cs

--- riders.cs
+++ riders.cs
@@ -6,13 +6,42 @@ namespace Project_Serval
         private string firstName;
         private string lastName;
         private string riderClass;
-    }
-    public riders RiderNumber
-    {
-        get { return RiderNumber; }
-        set 
-        { 
-            RiderNumber = int.Parse(value);
+
+        public void SetriderNumber(int riderNumber)
+        {
+            this.riderNumber = riderNumber;
         }
+        public int GetRiderNumber()
+        {
+            return riderNumber;
+        }
+        public void SetFirstName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+        public void SetLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+        public string GetLastName()
+        {
+            return lastName;
+        }
+        public void SetRiderClass(string riderClass)
+        {
+            this.riderClass = riderClass;
+        }
+        public string GetRiderClass()
+        {
+            return riderClass;
+        }
+
+
+
+
     }
 }
